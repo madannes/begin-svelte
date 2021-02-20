fe6f04af2cce0b876a5c0f5086f8792cd9e5c60e
madannes <madannes>
2021-02-20 11:53:33 -0500

    Quick changes to test CI+CD

--- src/App.svelte
+++ src/App.svelte
@@ -5,15 +5,13 @@
   onMount(async () => {
     let data = await (await fetch('/api')).json()
     message = data.msg
-    console.log('MESSAGE: ', message)
+    console.log('MESSAGE', message)
   })
 </script>
 
 <main>
   <h1>Hello {name}!</h1>
   <h2>{message}</h2>
-  <h3>Change me!</h3>
-  <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
 <style>
